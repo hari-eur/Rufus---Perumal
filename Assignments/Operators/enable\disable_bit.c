d7de02eee3761ea4af67d98bf6dcfc4067f69d8f
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-03-03 14:57:01 +0530

    Update enable\disable_bit.c

--- Assignments/Operators/enable\disable_bit.c
+++ Assignments/Operators/enable\disable_bit.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int num = 17; // binary 0001 0001
-    int pos = 3; // position of the bit to be modified
+int main() 
+{
+    int num=17; // 0001 0001
+    int pos=3; // position of the bit to be modified
 
     // enable the bit
-    num |= (1 << pos);
-    printf("After enabling bit %d: %d\n", pos, num); // binary 0001 1001
+    num |= (1<<pos);
+    printf("After enabling bit %d: %d\n",pos,num);// 0001 1001
 
     // disable the bit
-    num &= ~(1 << pos);
-    printf("After disabling bit %d: %d\n", pos, num); // binary 0001 0001
+    num &= ~(1<<pos);
+    printf("After disabling bit %d: %d\n",pos,num);// 0001 0001
 
     return 0;
 }
