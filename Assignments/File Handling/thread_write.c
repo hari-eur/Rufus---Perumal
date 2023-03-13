e39b946b48010e0a7e0e7770eced7b52960ed9c7
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-03-13 10:08:24 +0530

    Update thread_write.c

--- Assignments/File Handling/thread_write.c
+++ Assignments/File Handling/thread_write.c
@@ -1,37 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 void *write_to_file(void *arg)
- {
-    FILE *fp = fopen("dummyy.txt", "a"); 
-    if (fp==NULL) 
+{
+    FILE *fp = (FILE*) arg;
+    char *str = (char*) arg;
+
+    while(1)
+    {
+        fprintf(fp, "%s\n", str);
+        fflush(fp);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    FILE *fp = fopen("dummyy.txt", "a");
+    if (fp == NULL)
     {
         perror("Error opening file");
         exit(1);
     }
-    fprintf(fp,"%s\n", (char*) arg);
-    fclose(fp); 
-    pthread_exit(NULL);
-}
 
-int main() 
-{
     pthread_t thread1, thread2;
-    char *str1="Thread 1";
-    char *str2="Thread 2";
+    char *str1 = "Thread 1";
+    char *str2 = "Thread 2";
 
-    while (1) 
-    {
-        
-        pthread_create(&thread1, NULL,write_to_file,(void*)str1);
-        pthread_join(thread1,NULL);
+    pthread_create(&thread1, NULL, write_to_file, (void*)fp);
+    pthread_create(&thread2, NULL, write_to_file, (void*)fp);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
-        
-       pthread_create(&thread2,NULL, write_to_file,(void*)str2) ;  
-       pthread_join(thread2,NULL) ;
-    }
+    fclose(fp);
 
     return 0;
 }
+
