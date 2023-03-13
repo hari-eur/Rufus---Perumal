72cc483754be417f1a8c990cbefa29bfcd7dad0a
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-03-13 16:26:56 +0530

    Update INT_Signal.c

--- Assignments/Signals/INT_Signal.c
+++ Assignments/Signals/INT_Signal.c
@@ -5,17 +5,20 @@
 
 void sighandler(int);
 
-int main () {
-   signal(SIGINT, sighandler);
+int main () 
+{
+   signal(SIGINT,sighandler);
 
-   while(1){
+   while(1)
+   {
       printf("Going to sleep for a second...\n");
       sleep(1);
    }
    return(0);
 }
 
-void sighandler(int signum) {
-   printf("Caught signal %d, coming out...\n", signum);
+void sighandler(int signum)
+{
+   printf("Caught signal %d, coming out...\n",signum);
    exit(1);
 }
