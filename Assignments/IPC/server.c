6ef8b2f20f7467260f7ad6664b3033b883e19511
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-03-29 11:15:16 +0530

    Create server.c

---
+++ Assignments/IPC/server.c
@@ -0,0 +1,37 @@
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
+
+int main()
+{
+
+	// create server socket similar to what was done in
+	int servSockD=socket(AF_INET, SOCK_STREAM, 0);
+
+	// string store data to send to client
+	char serMsg[255]="Message from the server to the "
+					"client \'Hello Client\' ";
+
+	// define server address
+	struct sockaddr_in servAddr;
+
+	servAddr.sin_family=AF_INET;
+	servAddr.sin_port=htons(9001);
+	servAddr.sin_addr.s_addr=INADDR_ANY;
+
+	// bind socket to the specified IP and port
+	bind(servSockD, (struct sockaddr*)&servAddr,sizeof(servAddr));
+
+	// listen for connections
+	listen(servSockD, 1);
+
+	// integer to hold client socket.
+	int clientSocket=accept(servSockD, NULL, NULL);
+
+	// send's messages to client socket
+	send(clientSocket, serMsg, sizeof(serMsg), 0);
+
+	return 0;
+}
